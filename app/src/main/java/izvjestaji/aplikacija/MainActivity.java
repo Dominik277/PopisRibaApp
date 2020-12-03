f974c09790df3eb340e0a82af63384cd92c15843
Dominik277 <dominikdoric277@gmail.com>
2020-12-03 10:10:22 +0100

    Dodan Floating Action Button koji na klik prikazuje Snackbar sa porukom.

--- app/src/main/java/izvjestaji/aplikacija/MainActivity.java
+++ app/src/main/java/izvjestaji/aplikacija/MainActivity.java
@@ -1,13 +1,42 @@
 package izvjestaji.aplikacija;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
+
+import android.content.Context;
 import android.os.Bundle;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button button;
+    Context context;
+    CardView cardView;
+    ViewGroup.LayoutParams layoutParams;
+    TextView textView;
+    RelativeLayout relativeLayout;
+    FloatingActionButton floatingActionButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        floatingActionButton = findViewById(R.id.floatingActionButton);
+
+        floatingActionButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Snackbar.make(v,"Ovo je snackbar",Snackbar.LENGTH_LONG)
+                        .setAction("Action",null).show();
+            }
+        });
     }
 }
