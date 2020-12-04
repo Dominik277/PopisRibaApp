aaac5fa28101d0a76e69885ba14539e076e3926c
Dominik277 <dominikdoric277@gmail.com>
2020-12-04 13:15:28 +0100

    Dodana onClick metoda u CustomAdapter.

--- app/src/main/java/izvjestaji/aplikacija/CustomAdapter.java
+++ app/src/main/java/izvjestaji/aplikacija/CustomAdapter.java
@@ -2,11 +2,16 @@ package izvjestaji.aplikacija;
 
 import android.content.Context;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.ImageView;
 import android.widget.TextView;
+
+import com.google.android.material.snackbar.Snackbar;
+
 import java.util.ArrayList;
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 
 public class CustomAdapter extends ArrayAdapter<DataModel> implements View.OnClickListener{
 
@@ -29,6 +34,31 @@ public class CustomAdapter extends ArrayAdapter<DataModel> implements View.OnCli
     public void onClick(View v) {
 
         int position = (Integer) v.getTag();
+        Object object = getItem(position);
+        DataModel dataModel = (DataModel) object;
 
+        switch (v.getId()){
+            case R.id.slika:
+                Snackbar.make(v,"Release Date " +dataModel.getPrebivaliste()
+                        ,Snackbar.LENGTH_LONG).setAction("No action",null).show();
+                break;
+        }
+    }
+
+    private int lastPosition = -1;
+
+    @NonNull
+    @Override
+    public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+        //Get the data item for this position
+        DataModel dataModel = getItem(position);
+        //Check if an existing view is being reused,otherwise inflate the view
+        ViewHolder viewHolder;
+
+        final View result;
+
+        if (convertView == null){
+            viewHolder
+        }
     }
 }
